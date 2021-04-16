e7637bc0559a850a79d1fb2f95099d24d7721abf
Jason <jasontwuk@gmail.com>
2021-04-16 15:16:43 +0100

    add Dropdown.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React from "react";
-import Search from "./components/Search";
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
+// import Search from "./components/Search";
 // import Accordion from "./components/Accordion";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end JavaScript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end JavaScript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
+
+const options = [
+  { label: "The color Red", value: "red" },
+  { label: "The color Green", value: "green" },
+  { label: "A Shade of Blue", value: "blue" },
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectChange={setSelected}
+      />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectChange }) => {
+  const [open, setOpen] = useState(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // console.log(event.target);
+
+      // *** if ref.current contain the clicked element, then do nothing
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      // *** close dropdown
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
+
+  const renderOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onSelectChange(option);
+        }}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  // console.log(ref.current);
+
+  return (
+    <div className="ui form" ref={ref}>
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
